4b9815db786433b33bb2da14aed2575b734e9d4b
mandeepkanoujiya <mandeepdk123@gmail.com>
2020-05-20 10:30:32 +0530

    Updates

---
+++ precache-manifest.b8330bcd05684115b37e93d033085cc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb63dab64745651b1e036a6f8da5a47e",
+    "url": "/myfirstapp/index.html"
+  },
+  {
+    "revision": "adc12e79cbb4e3285791",
+    "url": "/myfirstapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "51167f7de473eda3f49d",
+    "url": "/myfirstapp/static/js/2.ffaa9bb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myfirstapp/static/js/2.ffaa9bb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc12e79cbb4e3285791",
+    "url": "/myfirstapp/static/js/main.4040a4be.chunk.js"
+  },
+  {
+    "revision": "90df23a462a6e41f2f0a",
+    "url": "/myfirstapp/static/js/runtime-main.b8f7c459.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myfirstapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
